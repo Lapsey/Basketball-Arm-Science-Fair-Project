903af5f59f95736eec81b764fa35abded190871d
Lapsey <64510365+Lapsey@users.noreply.github.com>
2020-06-20 11:06:02 -0700

    Update code.ino

--- code.ino
+++ code.ino
@@ -1,11 +1,28 @@
-int recordMove;
-int saveMove;
-int executeMove;
+const int recordPin = 0;
+const int executePin = 1;
+
+int recordMoveState = 0;
+int executeMoveState = 0;
+
+const int shoulderPotPin = 1;
+const int elbowPotPin = 2;
+const int wristPotPin = 3;
+
+int shoulderPosArr[2000] = {-1};
+int elbowPosArr[2000] = {-1};
+int wristPotArr[2000] = {-1};
+
+int index = 0;
 
 void setup() {
   
 }
 
 void draw() {
+  recordMoveState = digitalRead(recordPin);
+  executeMoveState = digitalRead(executePin);
   
+  while (recordMoveState == HIGH) {
+    
+  }
 }
